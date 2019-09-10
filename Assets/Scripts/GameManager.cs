482c43358a5dade23d95daa179ebb055be87d755
yosamin <shiro43min@gmail.com>
2019-09-10 13:35:11 +0900

    GameManagerの処理を改良

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,43 +9,65 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject goalPoint;
     private Rigidbody rigid;
 
+    private enum GameState{
+        ballStop, 
+        ballActive
+    }
+
+    private GameState gameState;
+
     // Start is called before the first frame update
     void Start()
     {
         rigid = ball.GetComponent<Rigidbody>();
-        StartCoroutine(StartGame());
-    }
-
-    private IEnumerator StartGame(){
-        rigid.useGravity = false;
-        rigid.isKinematic = true;
-        ball.transform.position = startPoint.transform.position;
-        while(!Input.GetMouseButtonDown(0)) yield return null;
-        rigid.useGravity = true;
-        rigid.isKinematic = false;
-        yield return StartCoroutine(GameLoop());
+        Reset();
+        StartCoroutine(GameLoop());
     }
 
     private IEnumerator GameLoop(){
         while(true){
             if(ball.transform.position.x < -20 || ball.transform.position.x > 20 
                 || ball.transform.position.y < -15 || ball.transform.position.y > 15){
-                Debug.Log("Reset");
-                StartCoroutine(StartGame());
-                break;
+                Reset();
             }
 
             if((ball.transform.position - goalPoint.transform.position).sqrMagnitude < 2){
                 StageClear();
                 break;
             }
+
+            if(Input.GetMouseButtonDown(0)) {
+                Debug.Log("mouse");
+                if(gameState == GameState.ballStop) {
+                    BallDrop();
+                }else if(gameState == GameState.ballActive){
+                    Reset();
+                }
+            }
+
             yield return null;
         }
+        yield break;
     }
 
     private void StageClear(){
         rigid.useGravity = false;
         rigid.isKinematic = true;
         Debug.Log("Clear");
+    }
+
+    private void Reset(){
+        rigid.useGravity = false;
+        rigid.isKinematic = true;
+        ball.transform.position = startPoint.transform.position;
+        gameState = GameState.ballStop;
+        Debug.Log("Reset:" + gameState);
+    }
+
+    private void BallDrop(){
+        rigid.useGravity = true;
+        rigid.isKinematic = false;
+        gameState = GameState.ballActive;
+        Debug.Log("Drop:" + gameState);
     }
 }
