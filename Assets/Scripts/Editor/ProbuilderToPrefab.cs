c2dc93fca7c1ad2c20ea6cbd71b2e597a24ae831
shoi <shoi.itou@gmail.com>
2019-09-10 17:45:21 +0900

    一つのステージを実際に遊べるところまで

---
+++ Assets/Scripts/Editor/ProbuilderToPrefab.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class ProbuilderToPrefab : MonoBehaviour
+{
+    [SerializeField]
+    GameObject obj;
+
+    [SerializeField]
+    private bool hoge;
+    private void Update()
+    {
+        if (hoge)
+        {
+            hoge = false;
+            CreatePrefabWithMeshInstance(obj);
+        }
+    }
+
+    public static void CreatePrefabWithMeshInstance(GameObject myMeshGO)
+    {
+        //プレハブ化する前のメッシュフィルター
+        MeshFilter meshFilter = myMeshGO.GetComponent<MeshFilter>();
+
+        //プレハブ化
+        GameObject prefab = PrefabUtility.CreatePrefab("Assets/Triangler.prefab", myMeshGO, ReplacePrefabOptions.Default);
+
+        //プレハブ化された後のメッシュフィルター
+        MeshFilter prefabMeshFilter = prefab.GetComponent<MeshFilter>();
+
+        //…にメッシュ参照をセットしなおす
+        prefabMeshFilter.sharedMesh = meshFilter.sharedMesh;
+
+        //メッシュデータそのものを同じプレハブの中に詰める
+        AssetDatabase.AddObjectToAsset(meshFilter.sharedMesh, prefab);
+
+        //アセットのアップデート
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+}
