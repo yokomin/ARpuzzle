e6f98866db7d5fa2516275ca891e2bbd4b56189e
yosamin <shiro43min@gmail.com>
2019-09-11 11:21:42 +0900

    死亡した場所にTombを出す

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -51,7 +51,7 @@ public class GameManager : MonoBehaviour
                 break;
             }
 
-            if(isDead) 
+            if(isDead)
                 Reset();
 
             if(Input.GetMouseButtonDown(0)) {
